7f1d87514cd4963488759ab19e44136508163063
Jeni Dang <jenipdang@yahoo.com>
2022-07-12 15:56:23 -0700

    update cart product model

--- app/models/cart_product.rb
+++ app/models/cart_product.rb
@@ -2,7 +2,7 @@ class CartProduct < ApplicationRecord
     belongs_to :user
     belongs_to :product
 
-    validates :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
+    validates :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 12 }
 
 
 end
